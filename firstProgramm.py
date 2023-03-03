3d21da33ec56122f08c2224cb742d204f76b73a6
Sthakur19 <STHAKUR@ibs-b.hu>
2023-03-03 20:33:27 +0530

    Add first python file

---
+++ firstProgramm.py
@@ -0,0 +1,43 @@
+
+
+# to_find_number = int(input("Please enter numer"))
+
+# if (to_find_number % 2 == 0):
+#     print("its odd numer")
+# elif to_find_number % 2 != 0:
+#     print("its even numer")
+# else:
+#     print("print nothing")
+
+# Write a program that reads a number form the standard input,
+# If the number is zero or smaller it should print: Not enough
+# If the number is one it should print: One
+# If the number is two it should print: Two
+# If the number is more than two it should print: A lot
+
+# standard_input = int(input("Please enter your special Number"))
+
+# if standard_input == 0 or standard_input < 0 :
+#     print("Not enough")
+# elif standard_input == 1:
+#     print("the number enter is 1")
+# elif standard_input == 2:
+#     print("the number enter is 2")
+# elif standard_input > 2:
+#     print(" th numer is allot")
+# else:
+#     print("noh")
+
+#Write a program that asks for two numbers and prints the bigger one
+
+# one_number = int(input("please enter numbrt one"))
+# second_number = int(input("please enter second number"))
+
+# if one_number > second_number:
+#     print("this number one is bigger")
+# elif second_number > one_number :
+#     print("this number second  is bigger")
+# else:
+#     print("both number are same")
+
+
