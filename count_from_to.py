4ce7976613bebb4889799cd6efd168a3017143e6
Sthakur19 <STHAKUR@ibs-b.hu>
2023-03-05 15:41:34 +0530

    Condioning Program

---
+++ count_from_to.py
@@ -0,0 +1,22 @@
+# Create a program that asks for two numbers
+# If the second number is not bigger than the first one it should print:
+# "The second number should be bigger"
+#
+# If it is bigger it should count from the first number to the second by one
+# 
+# example:
+#
+# first number: 3, second number: 6, should print:
+#
+# 3
+# 4
+# 5
+
+first_number = int(input("Enter the First Number here : "))
+second_number = int(input("Enter the Second Number here : "))
+
+if (second_number < first_number):
+    print("The Second number should be bigger")
+else:
+    for i in range (first_number, second_number):
+        print(i)
