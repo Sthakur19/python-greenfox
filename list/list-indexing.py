3490cbb04de0709ffe3f942846551f0244852230
Sthakur19 <STHAKUR@ibs-b.hu>
2023-03-15 10:47:11 +0100

    list-indexing.py

---
+++ list/list-indexing.py
@@ -0,0 +1,25 @@
+list = [1,3,5,"abc", [1,2,4]]
+
+print(list[2])
+
+print(list[3][0])
+
+print(len(list))
+
+print(list[len(list)-2])
+
+print(list[-1])
+
+print(list[1:3])
+
+print(list[3:])
+
+print(list[:3])
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [6,7,8]
+]
+
+print(matrix[1][2])
