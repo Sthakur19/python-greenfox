c2bca8b05958b498f3c89ebc3a01277246f93274
Sthakur19 <STHAKUR@ibs-b.hu>
2023-03-15 16:04:03 +0100

    sum_all.py

---
+++ list/sum_all.py
@@ -0,0 +1,8 @@
+
+# - Create a variable named `numbers`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Print the sum of the elements of `numbers`
+
+numbers = [3, 4, 5, 6, 7]
+sum_of_all = sum(numbers)
+print(sum_of_all)
