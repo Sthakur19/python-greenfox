7eeb2b0ddc0dc16e47db4d1e755759b187dbba42
Sthakur19 <STHAKUR@ibs-b.hu>
2023-03-25 17:59:34 +0100

    greet.py

---
+++ function/greet.py
@@ -0,0 +1,11 @@
+
+# 2 greet
+# Create variable named `al` and assign the value `Green Fox` to it
+# Create a function called `greet()` that greets its input parameter
+#    - It prints a greeting message e.g. `Greetings dear Green Fox`
+# Greet `al`
+
+al = "Green Fox"
+def greet(message):
+    return "Greetings dear " + message
+print(greet(al))
